d3f857ab6040d71fb4b3968eb985ce94833eed3b
Satori32 <Huga8989+githubf@gmail.com>
2021-03-14 18:05:45 +0900

    maybe divmod have bug.now.

--- ビット演算？？？/ソース.cpp
+++ ビット演算？？？/ソース.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <tuple>
 #include <limits>
-
+#include <algorithm>
+#include <cmath>
+#include <random>
 //https://ja.wikipedia.org/wiki/â¡éZäÌ
 
 bool And(const bool& A, const bool& B) {
@@ -86,7 +88,7 @@ T Sub(const T& A, const T& B) {
 }
 
 template<class T>
-T  Mul(const T& A,const T& B) {
+T  Mul(const T& A,const T& B) {//ïMéZñ@Ç≈Ç∑ÅB
 	T C(0);
 
 	for (std::size_t X = 0; X < std::numeric_limits<T>::digits; X++) {
@@ -95,6 +97,35 @@ T  Mul(const T& A,const T& B) {
 		}
 	}
 	return C;
+}
+
+template<class T>
+int Sign(const T& A) {
+	return A >= 0 ? 1 : -1;
+}
+
+template<class T>
+T Abs(const T& In) {
+	return In >= 0 ? In : -In;
+}
+
+template<class T>
+std::tuple<T, T> DivMod(const T& A, const T& B) {//ïMéZñ@ÉfÉXÅB
+	T Y(0);
+	T Z(Abs(A));
+	std::size_t i = 0;
+	if (B == 0) { return { 1,A }; }
+	if (Abs(A) <Abs(B)) { return { 0,A }; }
+	for (std::size_t j = 0; j < std::numeric_limits<T>::digits; j++) {
+		for (i = 0; i < std::numeric_limits<T>::digits; i++) {
+			if (Abs(Z) < (Abs(B) << i)) { break; }
+		}
+		Y |= 1 << (i - 1);
+		Z -= Abs(B) << (i - 1);
+		if (Abs(Z) < Abs(B)) { break; }
+	}
+
+	return { Y * Sign(Sign(A) * Sign(B)),Abs(Z) * Sign(Sign(A) * Sign(B)) };
 }
 
 
@@ -117,6 +148,33 @@ int main() {
 	int Z4 = Mul(A/2, A);	std::cout << "Mul:" << A/2 << '*' << A << "==" << Z4 << std::endl;
 	int Z5 = Mul(-A, -A);		std::cout << "Mul:" << -A << '*' << -A <<"=="<<Z5<< std::endl;
 	int Z6 = Mul(-A, A);		std::cout << "Mul:" << -A << '*' << A <<"=="<<Z6<< std::endl;
+
+	
+
+	auto W1 = DivMod(A, A);		std::cout << "Div:" << A << '/' << A << "=="<< std::get<0>(W1)<< std::endl;
+	auto W2 = DivMod(A, B);		std::cout << "Div:" << A << '/' << B << "=="<< std::get<0>(W2)<<std::endl;
+	auto W3 = DivMod(A, B/2);	std::cout << "Div:" << A << '/' << B/2 <<"=="<<std::get<0>(W3)<< std::endl;
+
+	std::size_t L = 32;
+
+	for (int i = 0; i < L; i++) {
+		for (int j = 1; j <= i; j++) {
+			auto X = DivMod(i, j);
+			std::cout << "Div:" << i << '/' << j <<"=="<<std::get<0>(X)<<','<<std::get<1>(X)<< std::endl;
+		}
+	}
+	std::size_t LL = 1024;
+
+	std::mt19937 mt(1);
+	std::uniform_int_distribution<int> ui(-1024, 1024);
+	for (std::size_t i = 0; i < LL; i++) {
+		auto X = ui(mt);
+		auto Y = ui(mt);
+		auto W = DivMod(X, Y);
+
+		std::cout << X << '/' << Y << ':' << X / Y << "==" << std::get<0>(W) <<':'<<((X/Y)==std::get<0>(W))<<','<< X % Y << "==" << std::get<1>(W) << std::endl;
+	}
+
 	return 0;
 
 }
